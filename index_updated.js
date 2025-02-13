929934ca8a42b7bfcd3c459349d424b7121f1cda
vlavar <145839850+VladimirVaraksin@users.noreply.github.com>
2025-02-13 21:28:40 +0100

    Update index_updated.js

--- index_updated.js
+++ index_updated.js
@@ -520,7 +520,9 @@ function checkInputFelder() {
                 }
             } else {
                 if (input.value.trim() === "margostkwismar12345") {
-                    const confirmation = confirm(`Margo ist nicht nur meine beste Freundin, sondern auch mein Fels in der Brandung.\nSie hat mir so oft geholfen, immer ein offenes Ohr und ein riesiges Herz. \nOhne sie wäre vieles nur halb so schön!`);
+                    confirm("Margo ist nicht nur meine beste Freundin, sondern auch mein Fels in der Brandung. \n" +
+                        "Sie hat mir so oft geholfen, immer ein offenes Ohr und ein riesiges Herz. \n" +
+                        "Ohne sie wäre vieles nur halb so schön! Bitte bestätigen, dass Margo topchik ist");
                 }
             }
         });
